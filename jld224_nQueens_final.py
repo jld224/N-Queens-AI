eea5d26ff53218453c3abae210fc84c94169acb0
dorito200 <jdarida9@gmail.com>
2023-09-18 10:39:44 -0400

    update readme and finish implementation

--- jld224_nQueens_final.py
+++ jld224_nQueens_final.py
@@ -1,5 +1,5 @@
 # CPSC 460/560 AI project 1 - Constrained N_Queens using backtracking
-# Constraint: 1st queen's position is fixed
+#                             Constraint: 1st queen's position is fixed
 # Author: your name
 #
 # Do not change the following part. NO other package is allowed.
@@ -48,17 +48,31 @@ def solveNQueensUtil(board, row, length):
                     return True
                 board[row][col] = 0
     else:
-        # If queen exists, move to the next row
         if solveNQueensUtil(board, row + 1, length):
             return True
 
     return False
+
+# Complete this part; add more functions if you need
+# @param: initial board with the 1st queen
+# @return: solution in a pandas.DataFrame
+def nQueens_solver(initialBoard):
+    size = len(initialBoard)
+    board = initialBoard.values.tolist()
+    if not solveNQueensUtil(board, 0, size):
+        return None
+    return pd.DataFrame(board)
+
+
+# No need to change the main function. Do not change output filename.
 def main():
     fileName_initial_Queen_pos, = sys.argv[1:]
     print('input filename: '+fileName_initial_Queen_pos)
     input_df = pd.read_csv(fileName_initial_Queen_pos, header=None)
-    solution_df = solveNQueensUtil(input_df, 0, 0)
-
+    #
+    # solution_df is the dataframe that stores your solution to the n-Queens problem (1-queen, 0-blank)
+    solution_df = nQueens_solver(input_df)
+    #
     if solution_df is None:
         print("No solution!")
     else:
@@ -66,5 +80,7 @@ def main():
         print('output file name: ' + output_fname)
         solution_df.to_csv(output_fname, sep=',', header=None, index=False)
 
+
 if __name__ == '__main__':
     main()
+
